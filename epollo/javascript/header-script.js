0b19be6cd89ef3ba7b0b303719b7d26a4a3bfe52
Daniel Roberts <BeardOfDan@gmail.com>
2018-03-26 22:32:29 -0700

    (feat) Show log in/out button
    Note: It is fairly glitchy in terms of displaying, otherwise
    though, it works fine.

--- epollo/javascript/header-script.js
+++ epollo/javascript/header-script.js
@@ -1,15 +1,12 @@
 $(document).ready(function () {
   $('#header').load('../pages/header.txt');
 
-  // hide/show log in/out links
+  // show log in/out link
   $.get('/auth/currentUser', (response) => {
     if (response === false) { // not logged in
       $('#log-in-with-google').show();
-      $('#log-out').hide();
     } else { // logged in
-      $('#log-in-with-google').hide();
       $('#log-out').show();
     }
   });
-
 });
