2fcb6ba583102443ff89e01530f42855b4a600a9
Daniel Roberts <BeardOfDan@gmail.com>
2018-03-26 22:01:18 -0700

    (feat) Authentication status governs log in/out buttons
    When header-script.js loads, it checks /auth/currentUser to see
    if the user is logged in or not. It then shows/hides the
    correct anchor tags.

--- epollo/javascript/header-script.js
+++ epollo/javascript/header-script.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $('#header').load('../pages/header.txt');
+
+  // hide/show log in/out links
+  $.get('/auth/currentUser', (response) => {
+    if (response === false) { // not logged in
+      $('#log-in-with-google').show();
+      $('#log-out').hide();
+    } else { // logged in
+      $('#log-in-with-google').hide();
+      $('#log-out').show();
+    }
+  });
+
 });
